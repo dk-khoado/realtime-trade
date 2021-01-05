2cd2b6e2d222ffab6565708e29b8bd69cc220a0a
khoa do <violent12330@gmail.com>
2021-01-05 15:38:22 +0700

    uptd

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+require('dotenv').config()
 var db = require("./models/indexDB")
 
 var indexRouter = require('./routes/index');
