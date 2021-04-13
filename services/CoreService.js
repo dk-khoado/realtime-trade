580b93a1651bc28b05f55b191db52216dc4f7d1b
Ngô Quốc Duy <duy.haivl321@gmail.com>
2021-04-13 13:06:46 +0700

    Duy

--- services/CoreService.js
+++ services/CoreService.js
@@ -5,14 +5,6 @@ class CoreService extends ServiceBase {
     constructor(model) {
         super(model);
     }
-    async get_balance(user_id, email) {
-        try {
-            let balance = await this.model.findOne({ user_id: user_id, email: email })
-            return new Response(false, balance);
-        } catch (error) {
-            return new Response(true, error, "Error");
-        }
-    }
 }
 
 module.exports = { CoreService }
